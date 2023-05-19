2596f6b1559068d939a5401bf99617ed8ea39d02
Harshith <harshithgv.sangamone@gmail.com>
2023-05-19 15:58:29 +0530

    token changed

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const [videoCall, setVideoCall] = useState(true);
 const rtcProps = {
     appId: "c924bd6fb57b44b1809c0746b78875d8",
     channel: "Navatar",
-    token: "007eJxTYDi85mXDBKtgvt0M24+pGryKfXP5XMn+HsmqH9ZzLvmahxkqMCRbGpkkpZilJZmaJ5mYJBlaGFgmG5ibmCWZW1iYm6ZYKJsmpTQEMjIc+/uSkZEBAkF8dga/xLLEksQiBgYA88IhXw==",
+    token: "007eJxTYDjaFXN+XejlvXeqVpx3UeIVOcH1ZX8462blPxev6apItasqMCRbGpkkpZilJZmaJ5mYJBlaGFgmG5ibmCWZW1iYm6ZYrFJPT2kIZGSQc9nOyMgAgSA+O4NfYlliSWIRAwMAue8fug==",
 };
 const callbacks = {
   EndCall: () => setVideoCall(false),
